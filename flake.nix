1c92181afb61d689eb11a2792ea7a507c6979761
ymgyt <yamaguchi7073xtt@gmail.com>
2026-01-12 17:21:04 +0900

    feat: add hello module

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Out-of-tree Linux drivers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      # how to set this value properlly?
+      kDir = "/nix/store/nx4mdfzx7rkwl9zkqspmfcxxznd92akj-linux-6.12.63-dev/lib/modules/6.12.63/build";
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        K_DIR = kDir;
+
+        shellHook = ''
+          exec nu
+        '';
+      };
+    };
+}
